588bdc728ecb9c0b4460af3c4a09ab95e3192bb1
Philipp Klein <philipp.klein@ipt.fraunhofer.de>
2023-12-19 08:49:42 +0100

    change init method and draft for bullet

--- main.go
+++ main.go
@@ -2,53 +2,56 @@ package main
 
 import (
 	"log"
-	"image/color"
+	//"image/color"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
+
 const (
-	screenWidth  = 320
-	screenHeight = 240
+	screenWidth    = 320
+	screenHeight   = 240
+	shipSingleMove = 3
+	bulletOffset   = 5
 )
-var ship *Ship
-// init the image
-func init() {
-	var XPos int16 = screenWidth/2
-	var YPos int16 = screenHeight
 
-	ship = CreateShip(XPos, YPos)
-
-
+type Game struct {
+	ship   *Ship
+	bullet *ShootingBullet
 }
-type Game struct{}
 
 func (g *Game) Update() error {
-	// movement of ship 
+	// movement of ship
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
 		// shoot
-		return nil
+		g.Shoot()
 	}
-	if inpututil.KeyPressDuration(ebiten.KeyRight) >0 {
+	if inpututil.KeyPressDuration(ebiten.KeyRight) > 0 {
 		// move left
-		g.MoveShip(3)
+		g.MoveShip(shipSingleMove)
 	}
-	if inpututil.KeyPressDuration(ebiten.KeyLeft)>0 {
+	if inpututil.KeyPressDuration(ebiten.KeyLeft) > 0 {
 		// move right
-		g.MoveShip(-3)
+		g.MoveShip(-shipSingleMove)
 	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{0x29, 0x62, 0xff, 0xff})
+	//screen.Fill(color.RGBA{0x29, 0x62, 0xff, 0xff})
 	// draw the ship:
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(0.5, 0.5)
-	op.GeoM.Translate(float64(ship.XPos), float64(ship.YPos))
-	op.GeoM.Translate(-float64(ship.Img.Bounds().Dx())/4, -float64(ship.Img.Bounds().Dy())/2)
-	screen.DrawImage(ship.Img, op)
+	op.GeoM.Translate(float64(g.ship.XPos), float64(g.ship.YPos))
+	op.GeoM.Translate(-float64(g.ship.Img.Bounds().Dx())/4, -float64(g.ship.Img.Bounds().Dy())/2)
+	screen.DrawImage(g.ship.Img, op)
+
+	// draw the bullet:
+	if g.bullet.visible {
+		// TODO: adjust the initial position
+		screen.DrawImage(g.bullet.Img, op)
+	}
 
 	ebitenutil.DebugPrint(screen, "Space Invaders")
 }
@@ -56,18 +59,33 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-func (g *Game) MoveShip(steps int16) error{
-	newPos := ship.XPos + steps
-	if 0 <= float64(newPos) - float64(ship.Img.Bounds().Dx())/4 && float64(newPos) +float64(ship.Img.Bounds().Dx())/4 <= screenWidth {
-		ship.XPos = newPos
+func (g *Game) MoveShip(steps int16) {
+	newPos := g.ship.XPos + steps
+	if 0 <= float64(newPos)-float64(g.ship.Img.Bounds().Dx())/4 && float64(newPos)+float64(g.ship.Img.Bounds().Dx())/4 <= screenWidth {
+		g.ship.XPos = newPos
 	}
-	return nil
+}
+
+func (g *Game) Shoot() {
+	// start shooting process
+	g.bullet.visible = true
 }
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("Space Invaders Go")
+
+	// init the game:
+	var ship *Ship
+	var bullet *ShootingBullet
+	var XPos int16 = screenWidth / 2
+	var YPos int16 = screenHeight
+	ship = CreateShip(XPos, YPos)
+	bullet = CreateBullet(XPos, YPos+bulletOffset)
+
+	g := &Game{ship, bullet}
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
--- ship.go
+++ ship.go
@@ -5,28 +5,50 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-const ShipImagePath string = "img/Ship.png"
+const (
+	ShipImagePath string = "img/Ship.png"
+	BulletImagePath string = "img/Bullet.png"
+)
 
 type Ship struct{
 	Img *ebiten.Image
 	XPos int16
 	YPos int16
 }
-func CreateShip(XPos, YPos int16) *Ship{
+func CreateShip(XPos, YPos int16) *Ship {
 	var err error
 	var img *ebiten.Image
 	img, _, err = ebitenutil.NewImageFromFile(ShipImagePath)
 	if err != nil { 
 		log.Fatal(err)
 	}
-	return &Ship{Img: img, XPos: XPos, YPos: YPos}
+	return &Ship{
+		Img: img, 
+		XPos: XPos, 
+		YPos: YPos,
+	}
 }
 
-type ShootingParticle struct{
+type ShootingBullet struct {
+	Img *ebiten.Image
 	XPos int16
 	YPos int16
+	visible bool
 }
 
-func SpawnParticle(ShipXPos, ShipYPos int16){
-	// bla
+func CreateBullet(XPos, YPos int16) *ShootingBullet {
+	var err error
+	var img *ebiten.Image
+	img, _, err = ebitenutil.NewImageFromFile(BulletImagePath)
+	if err != nil { 
+		log.Fatal(err)
+	}
+	return &ShootingBullet{
+		Img: img, 
+		XPos: XPos, 
+		YPos: YPos,
+		visible: false,
+	}
 }
+
+// --> way to increment bullet movement
